c10a30069135bf1152cf616b833f6c02e55fd638
Kan <kanyakancat@gmail.com>
2023-11-15 14:57:10 +0300

    search

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useState } from 'react'
 import styles from './Main.module.scss'
 
 const Main = () => {
+	const [checkedName, setCheckedName] = useState(false)
+	const [checkedType, setCheckedType] = useState(false)
+
+	function changeCheckboxName() {
+		setCheckedName(!checkedName)
+	}
+
+	function changeCheckboxType() {
+		setCheckedType(!checkedType)
+	}
+
 	return (
 		<div className={styles.root}>
 			<div className={styles.main}>
@@ -22,23 +33,62 @@ const Main = () => {
 			</div>
 
 			<div className={styles.search}>
-				<h4>Искать предмет</h4>
+				<h4>Искать предмет [?]</h4>
+				<div className={styles.searchWrapper}>
+					<form className={styles.form}>
+						<label className={styles.formTitle}>Название</label>
+						<input />
+						<div className={styles.input}>
+							<input
+								type='checkbox'
+								name='name'
+								id='name'
+								checked={checkedName}
+								onChange={changeCheckboxName}
+							/>
+							<label for='name' className={styles.label}>
+								именно такое название
+							</label>
+						</div>
+						<div className={styles.input}>
+							<input
+								type='checkbox'
+								name='type'
+								id='type'
+								checked={checkedType}
+								onChange={changeCheckboxType}
+							/>
+							<label for='type' className={styles.label}>
+								только один тип
+							</label>
+						</div>
+						<button type='submit'>
+							<p>искать</p>
+						</button>
+					</form>
+				</div>
 
-				<form className={styles.item}>
-					<label>Название</label>
-					<input />
-					<div>
-						<input type='radio' />
-						<label>именно такое название</label>
-					</div>
-					<div>
-						<input type='radio' />
-						<label>только один тип</label>
-					</div>
-					<button type='submit'>
-						<p>искать</p>
-					</button>
-				</form>
+				<h4>Искать игрока</h4>
+				<div className={styles.searchWrapper}>
+					<form className={styles.form}>
+						<label className={styles.formTitle}>Имя</label>
+						<input />
+						<button type='submit'>
+							<p>искать</p>
+						</button>
+					</form>
+					<form className={styles.form}>
+						<label className={styles.formTitle}>Предмет в вишлисте</label>
+						<input />
+						<button type='submit'>
+							<p>искать</p>
+						</button>
+					</form>
+				</div>
+
+				<button className={styles.reset}>
+					<p>сбросить</p>
+				</button>
 			</div>
 		</div>
 	)
