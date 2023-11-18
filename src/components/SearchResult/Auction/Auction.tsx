5f6c6fa814797c9fd5217b6a3a8bb2da06a7110d
Kan <kanyakancat@gmail.com>
2023-11-18 12:34:10 +0300

    auction

--- src/components/SearchResult/Auction/Auction.tsx
+++ src/components/SearchResult/Auction/Auction.tsx
@@ -26,16 +26,26 @@ const Auction = () => {
 										<b>Тип</b>: Нямочка
 									</span>
 									<span className={styles.subtext}>
-										<b>Редкость</b>: Особый
+										<b> Редкость</b>: Особый
 									</span>
 								</div>
 							</div>
 						</div>
-						<div className={styles.price}>10 000</div>
+						<div className={styles.price}>
+							<span className={styles.priceNum}>
+								10 000
+								<img
+									src='https://s3-alpha-sig.figma.com/img/d420/837c/b0d85e89e03259573abe526d32efad92?Expires=1701043200&Signature=STLOkIpQukW~xZv3NwmJ78G8KhFkMc7FQd8A-SJTOp9CzABCIfpVV0~aMtp6VxEM2K2Wc12hEtM0FN3pAsazYumDnxePZHRu4peSE4-c0N8sYnDoJmwq2164bHDA2mohsKmyHH~YytbHXU8Qqf0umvH-Du5kBQ5pUcojWHm1-~209lOS8hxNCwVU-CI7kF3wJKpz3pT1B4PDC~V5OruSVx-6XHeTdw5OLsRzvJei0lxs44SOeyPPyXpRmQ6CUcNfJOEWRfKtNaLG8ZDQJKRXWrLqqomihP-2eG9FRICpB6EegNOkMAHNbjTGBgfNGT3DIdNoz-v5HvxTgeVOh7n1IA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+									alt=''
+								/>
+							</span>
+						</div>
 						<div className={styles.trade}>
 							<form action='submit'>
-								<input type='text' />
-								<button type='submit'>предложить</button>
+								<input type='text' className={styles.input} />
+								<button type='submit' className={styles.btn}>
+									предложить
+								</button>
 							</form>
 						</div>
 					</div>
